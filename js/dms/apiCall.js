170f87c418ef78676f01b53349fc4a984b740fcc
lenovo <lenovo>
2017-04-30 02:06:16 +0800

    get the response from the json

--- js/dms/apiCall.js
+++ js/dms/apiCall.js
@@ -7,12 +7,11 @@ $(document).ready(function() {
     function getPatientList(){
         var patientResult;
         $.getJSON("/v1/fetchallpatient", function(data){
-            var key, value;
+            console.log("Data fetch: " + JSON.stringify(data));
             var patientCount = 0;
             for (var key in data) {
-                if(key.hasOwnProperty(key)){
+                if(data.hasOwnProperty(key)){
                     patientCount++;
-                    console.log(patientCount);
                     var value = data[key];
                     console.log("Data: " + JSON.stringify(value));
                     patientResult = JSON.stringify(value);
